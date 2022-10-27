a9d6505de4275289fde8605f64d3703c22fbde4d
Zahy Padilla Orrala <zlpadillao@hotmail.com>
2022-10-27 10:32:57 -0500

    segundo

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ test();
 function test() {
     console.log('Test-Gib');
     console.log('Este proyecto es relacionado');
-    console.log('Este proyecto es relacionado');
     console.log('no funca');
 }
