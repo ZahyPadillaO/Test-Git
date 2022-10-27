cca932d3602c28c6ae69c336b5b9c7c0823a94e7
ZahyPadillaO <85964750+ZahyPadillaO@users.noreply.github.com>
2022-10-27 11:02:06 -0500

    Delete gib directory

--- gib/logger/main.js
+++
@@ -1,9 +0,0 @@
-test();
-
-function test() {
-    console.log('Test-Gib');
-    console.log('Test-Gib');
-    console.log('Este proyecto es relacionado');
-    console.log('no funca');
-    console.log('no funca');
-}
--- gib/main.js
+++
@@ -1,8 +0,0 @@
-test();
-
-function test() {
-    console.log('Test-Gib ayuda');
-    console.log('Este proyecto es relacionado');
-    console.log('Este proyecto es relacionado');
-    console.log('no funca');
-}
