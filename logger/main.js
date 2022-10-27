c285a1e3d4bf779f53deaef43fb71ef3332e549a
Zahy Padilla Orrala <zlpadillao@hotmail.com>
2022-10-27 15:38:45 -0500

    7898

--- logger/main.js
+++ logger/main.js
@@ -3,6 +3,5 @@ logger();
 function logger() {
     console.log('Test-Gib logger');
     console.log('Este proyecto es relacionado');
-    console.log('Este proyecto es relacionado');
     console.log('no funca');
 }
