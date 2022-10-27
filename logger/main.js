2a087abfb3110edf549e41b07cae47c1e6b0af62
Zahy Padilla Orrala <zlpadillao@hotmail.com>
2022-10-27 12:32:17 -0500

    test

--- logger/main.js
+++ logger/main.js
@@ -1,7 +1,7 @@
 logger();
 
 function logger() {
-    console.log('Test-Gib');
+    console.log('Test-Gib logger');
     console.log('Este proyecto es relacionado');
     console.log('no funca');
 }
--- test/main.js
+++ test/main.js
@@ -1,7 +1,7 @@
 test();
 
 function test() {
-    console.log('Test-Gib ayuda');
+    console.log('Test-Gib ayuda test');
     console.log('Este proyecto es relacionado');
     console.log('no funca');
 }
